f4825290d1f59145e787d04c551572f02ba2d6d7
mickyelmb <mickyel50@gmail.com>
2022-11-13 10:34:32 -0500

    Implement the Add button using match/case coding

--- gui.py
+++ gui.py
@@ -2,9 +2,22 @@ import functions
 import PySimpleGUI as sgui
 
 label = sgui.Text("Type in a to-do")
-input_box = sgui.InputText(tooltip="Enter todo")
+input_box = sgui.InputText(tooltip="Enter todo", key="todo")
 add_button = sgui.Button("Add")
 
-window = sgui.Window('My To-Do App', layout=[[label, input_box, add_button]])
-window.read()
+window = sgui.Window('My To-Do App',
+                     layout=[[label], [input_box, add_button]],
+                     font=('Helvetica', 15))
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sgui.WIN_CLOSED:
+            break
 window.close()
